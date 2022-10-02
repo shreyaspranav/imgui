88f29d53dbe93bf9efad8696c98a28b9b1a90ddb
shreyaspranav <shrepranav5@gmail.com>
2022-10-02 15:30:11 +0530

    deleted examples and updated premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,54 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "On"
+
+	targetdir ("%{wks.location}/bin/" .. bin_folder .. "/")
+	objdir ("%{wks.location}/bin/" .. bin_folder .. "/obj")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"imgui_tables.cpp",
+
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp",
+		"backends/imgui_impl_opengl3_loader.h",
+	}
+
+	links { "GLFW" }
+
+	includedirs
+	{
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glad}",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release_Debug"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Production"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
